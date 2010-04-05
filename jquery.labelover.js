524105a42486ff3b808de6f3a52738bcaeecab2e
Paul <paul@paularmstrongdesigns.com>
2010-04-04 20:46:50 -0700

    initial commit. working demo.

---
+++ jquery.labelover.js
@@ -0,0 +1,29 @@
+(function($) {
+    
+    $.fn.labelover = function() {
+        
+        this.each(function() {
+            var $this = $(this);
+            var $label = $this.prev('label');
+            
+            $this.parent().addClass('labelover');
+            
+            $this.focus(function() {
+                if($this.val() == '') {
+                    $label.stop().animate({ opacity: 0.4 }, 200);
+                }
+            }).blur(function() {
+                if($this.val() == '') {
+                    $label.stop().css({ display: 'block' }).animate({ opacity: 1 }, 200);
+                }
+            }).keydown(function() {
+                if($label.css('opacity')) {
+                    $label.stop().animate({ opacity: 0 }, 200, function() { $label.css({ display: 'none' }); });
+                }
+            });
+            
+        });
+        
+    }
+    
+})(jQuery);
